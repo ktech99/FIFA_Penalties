173548ee958e3acd280f5f93b793a7eaa5403aa7
Kartik Arora <kartikar@uw.edu>
2019-03-07 21:21:03 -0800

    add charts for sid

---
+++ summary.r
@@ -0,0 +1,23 @@
+library(ggplot2)
+library(plyr)
+
+
+shotData = read.csv("shots.csv", header = TRUE)
+colnames(shotData)[1] <- "Sid"
+
+sid_data = shotData[, c("Sid")]
+
+ggplot(data.frame(sid_data), aes(x = sid_data, fill = sid_data)) +
+  geom_bar() + xlab("Direction") + ylab("Shots taken") + guides(fill = FALSE)
+
+sid_data = data.frame(sid_data)
+colnames(sid_data)[1] <- "Sid"
+
+#Right is 1, Center is 2, Left is 3
+sid_data$Sid <- revalue(sid_data$Sid, c("R"=1, "L"=3, "C"=2))
+
+x <- 1:length(sid_data$Sid)
+plot(x, sid_data$Sid, main = "Main title",
+     xlab = "X axis title", ylab = "Y axis title",
+      frame = FALSE)
+lines(x, sid_data$Sid, type = "l")
